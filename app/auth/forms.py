1a6097f3baaba992769e64c575a8948a5fb81633
denver1500 <denvtr@gmail.com>
2022-08-05 02:39:06 -0400

    Flask Microblog with SQLite and Elasticsearch

---
+++ app/auth/forms.py
@@ -0,0 +1,41 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+from flask_babel import _,lazy_gettext as _l
+
+class LoginForm(FlaskForm):
+    username=StringField(_l('Username'),validators=[DataRequired()])
+    password=PasswordField(_l('Password'),validators=[DataRequired()])
+    remember_me=BooleanField(_('Remember me'))
+    submit=SubmitField(_l('Sign In'))
+
+class RegistrationForm(FlaskForm):
+    username=StringField(_l('Username'),validators=[DataRequired()])
+    email=StringField(_l('Email'),validators=[DataRequired(),Email()])
+    password=PasswordField(_('Password'),validators=[DataRequired()])
+    password2=PasswordField(
+            _l('Repeat Password'),validators=[DataRequired(),EqualTo('password')])
+    submit=SubmitField(_l('Register'))
+
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(_l('Please use a different username.'))
+
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError(_l('Please use a different email address.'))
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField(_l('Email'), validators=[DataRequired(), Email()])
+    submit = SubmitField(_l('Request Password Reset'))
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField(_l('Password'), validators=[DataRequired()])
+    password2 = PasswordField(
+        _l('Repeat Password'), validators=[DataRequired(),
+                                           EqualTo('password')])
+    submit = SubmitField(_l('Request Password Reset'))
